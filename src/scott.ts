40515a84ea5b2352b57732cf6d53ab62076e9a38
whamer100 <whammer100@aol.com>
2019-11-15 16:27:57 -0800

    pls

--- src/scott.ts
+++ src/scott.ts
@@ -2,7 +2,7 @@ const badwords: string[] = ["of", "of a", "for", "the"]
 
 const correct = (s: string) => {
   const ns = (s.endsWith(" - Scott The Woz")) 
-    ? s.slice(0, 0 - " - Scott The Woz".length) : s
+    ? s.slice(0, 0 - " - Scott The Woz".length) : s.trim()
   for (const word in badwords) {
     if (ns.endsWith(word)) {
       const news = ns.substring(0, s.length - word.length)
